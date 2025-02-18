674fe126dae784e965ab24ca053c98533693fb86
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-02-18 14:54:08 -0600

    Change button colors to blue

--- gui.py
+++ gui.py
@@ -18,28 +18,32 @@ class App(customtkinter.CTk):
         self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
         self.resizable(False, False)
 
+        self.top_frame = customtkinter.CTkFrame(self, width=800, height=100)
+        self.top_frame.place(x=0, y=0)
+
+
         self.Frame1 = customtkinter.CTkFrame(self, width=800, height=240)
         self.Frame1.place(x=0, y=360)
 
-        self.odometer_button = customtkinter.CTkButton(self, bg_color=['gray86', 'gray17'], text="Current Odometer Reading", hover=False, font=customtkinter.CTkFont('Roboto', size=13, weight='bold'))
+        self.odometer_button = customtkinter.CTkButton(self, bg_color=['gray86', 'gray17'],fg_color="blue", text="Current Odometer Reading", hover=False, font=customtkinter.CTkFont('Roboto', size=13, weight='bold'))
         self.odometer_button.place(x=80, y=380)
 
         self.odometer_entry = customtkinter.CTkEntry(self, bg_color=['gray86', 'gray17'], width=160)
         self.odometer_entry.place(x=80, y=420)
 
-        self.fuel_price_button = customtkinter.CTkButton(self, bg_color=['gray86', 'gray17'], text="Fuel Price", hover=False, width=160, font=customtkinter.CTkFont('Roboto', size=13, weight='bold'))
+        self.fuel_price_button = customtkinter.CTkButton(self, bg_color=['gray86', 'gray17'],fg_color="blue", text="Fuel Price", hover=False, width=160, font=customtkinter.CTkFont('Roboto', size=13, weight='bold'))
         self.fuel_price_button.place(x=80, y=480)
 
         self.fuel_price_entry = customtkinter.CTkEntry(self, bg_color=['gray86', 'gray17'], width=160)
         self.fuel_price_entry.place(x=80, y=520)
 
-        self.total_gallons_button = customtkinter.CTkButton(self, bg_color=['gray86', 'gray17'], text="Total Gallons Purchased", hover=False, width=160, font=customtkinter.CTkFont('Roboto', size=13, weight='bold'))
+        self.total_gallons_button = customtkinter.CTkButton(self, bg_color=['gray86', 'gray17'],fg_color="blue", text="Total Gallons Purchased", hover=False, width=160, font=customtkinter.CTkFont('Roboto', size=13, weight='bold'))
         self.total_gallons_button.place(x=320, y=380)
 
         self.Entry3 = customtkinter.CTkEntry(self, bg_color=['gray86', 'gray17'], width=160)
         self.Entry3.place(x=320, y=420)
 
-        self.total_cost_button = customtkinter.CTkButton(self, bg_color=['gray86', 'gray17'], text="Total Cost", hover=False, width=160, font=customtkinter.CTkFont('Roboto', size=13, weight='bold'))
+        self.total_cost_button = customtkinter.CTkButton(self, bg_color=['gray86', 'gray17'],fg_color="blue", text="Total Cost", hover=False, width=160, font=customtkinter.CTkFont('Roboto', size=13, weight='bold'))
         self.total_cost_button.place(x=320, y=480)
 
         self.total_cost_entry = customtkinter.CTkEntry(self, bg_color=['gray86', 'gray17'], width=160)
@@ -48,7 +52,7 @@ class App(customtkinter.CTk):
         self.submit_button = customtkinter.CTkButton(self, bg_color=['gray86', 'gray17'], font=customtkinter.CTkFont('Roboto', size=26, weight='bold'), height=50, text="Submit", fg_color="#00b900", text_color="#000000", command=self.handle_submit_button)
         self.submit_button.place(x=655, y=545)
 
-        self.date_picker_label = customtkinter.CTkButton(self, bg_color=['gray86', 'gray17'], text="Select a Date", hover=False, width=160, font=customtkinter.CTkFont('Roboto', size=13, weight='bold'))
+        self.date_picker_label = customtkinter.CTkButton(self, bg_color=['gray86', 'gray17'],fg_color="blue", text="Select a Date", hover=False, width=160, font=customtkinter.CTkFont('Roboto', size=13, weight='bold'))
         self.date_picker_label.place(x=550, y=380)
 
         self.date_combobox = customtkinter.CTkComboBox(self)
@@ -56,8 +60,7 @@ class App(customtkinter.CTk):
 
         CTkDatePicker(self.date_combobox)
 
-        self.top_frame = customtkinter.CTkFrame(self, width=800, height=100)
-        self.top_frame.place(x=0, y=0)
+
 
         # Check if the CSV file exists before initializing the DataFrame
         if not os.path.isfile("fill_ups.csv"):
@@ -73,6 +76,7 @@ class App(customtkinter.CTk):
             odometer = int(odometer)
         except ValueError:
             print("Odometer reading must be a number. Please try again.")
+            self.odometer_button.configure(fg_color="red")
             #Clear the entry
             self.odometer_entry.delete(0, tkinter.END)
             return
@@ -83,8 +87,11 @@ class App(customtkinter.CTk):
             if int(odometer) < fill_ups["Odometer"].iloc[-1]:
                 print("Odometer reading must be greater than the previous reading. Please try again.")
                 #Clear the entry
+                self.odometer_button.configure(fg_color="red")
                 self.odometer_entry.delete(0, tkinter.END)
                 return
+            else:
+                self.odometer_button.configure(fg_color="blue")
 
         fuel_price = self.fuel_price_entry.get()
         # Check if the entry is a number or float
