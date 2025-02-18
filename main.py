e80d7dd2d9bdcb602b92c85f9ab3d5c48bb4f37d
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-02-18 09:13:25 -0600

    Add ability to edit stored vehicle information

--- main.py
+++ main.py
@@ -41,10 +41,41 @@ class App:
         with open('vehicle_info.json', 'w') as json_file:
             json.dump(vehicle_info, json_file, indent=4)
 
+    def edit_vehicle_info(self):
+        """Allows the user to edit the vehicle information"""
+        try:
+            with open('vehicle_info.json', 'r') as json_file:
+                vehicle_info = json.load(json_file)
+        except FileNotFoundError:
+            print("No existing vehicle information found.")
+            return
+
+        print("What information do you need to update about your vehicle?")
+        info_choice = input("1 - Model\n2 - Brand\n3 - Year\n4 - Tank Capacity\n5 - Odometer\n")
+        if info_choice == "1":
+            vehicle_info["model"] = input("Enter the model of the vehicle: ")
+        elif info_choice == "2":
+            vehicle_info["brand"] = input("Enter the brand of the vehicle: ")
+        elif info_choice == "3":
+            vehicle_info["year"] = input("Enter the year of the vehicle: ")
+        elif info_choice == "4":
+            vehicle_info["tank_capacity"] = input("Enter the tank size of the vehicle: ")
+        elif info_choice == "5":
+            vehicle_info["odometer"] = input("Enter the odometer reading of the vehicle: ")
+        else:
+            print("Invalid choice. Please try again.")
+            self.edit_vehicle_info()
+            return
+
+        with open('vehicle_info.json', 'w') as json_file:
+            json.dump(vehicle_info, json_file, indent=4)
+
+
+
 
 if __name__ == '__main__':
     app = App()
-    app.get_vehicle_info()
+    app.edit_vehicle_info()
 
 
 
