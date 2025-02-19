2024d1f11c3be7cdeddc257e2974f6219e368090
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-02-19 10:54:15 -0600

    Remove fill-ups.csv initializer from main.py

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import os
 import pandas as pd
 from gui import App
 
-# Check if the CSV file exists before initializing the DataFrame
-if not os.path.isfile("fill_ups.csv"):
-    fill_ups = pd.DataFrame(columns=["Nickname","Date", "Odometer", "Gallons", "Gas Price", "Total Cost"])
-    fill_ups.to_csv("fill_ups.csv", index=False)
 
 if __name__ == '__main__':
     app = App()
