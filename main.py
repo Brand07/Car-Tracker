93b767020278cae659401ac945e596c525e6a792
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-02-19 14:40:57 -0600

    Create Resource folder upon first run

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 import pandas as pd
 from gui import App
 
+#Create a Resources folder if it doesn't exist
+if not os.path.isdir("Resources"):
+    os.mkdir("Resources")
 
 if __name__ == '__main__':
     app = App()
