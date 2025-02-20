bf5b00a84fac6fdee1bf2d0d121364ec4c668087
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-02-20 13:42:30 -0600

    Refactor main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
-from gui import App
+from window import Window
 
 # Create a Resources folder if it doesn't exist
 if not os.path.isdir("Resources"):
     os.mkdir("Resources")
 
+
+
 if __name__ == "__main__":
-    app = App()
+    app = Window()
     app.mainloop()
