b996f5cabae4e406cad05c388e30742b4d4a782a
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-02-20 13:42:08 -0600

    Create/Update new file

---
+++ functions.py
@@ -0,0 +1,148 @@
+import os
+import pandas as pd
+import tkinter
+
+def insert_into_info_box(app, text):
+    app.info_box.configure(state="normal")
+    app.info_box.insert(1.0, text)
+    app.info_box.configure(state="disabled")
+
+def clear_info_box(app):
+    app.info_box.configure(state="normal")
+    app.info_box.delete(1.0, tkinter.END)
+    app.info_box.configure(state="disabled")
+
+def load_vehicles(app):
+    if os.path.isfile("Resources/vehicles.json"):
+        try:
+            vehicles = pd.read_json("Resources/vehicles.json")
+            vehicle_names = [str(name) for name in vehicles["Nickname"].tolist()]
+            app.vehicle_combo.configure(values=vehicle_names)
+        except ValueError:
+            print("Error: vehicles.json is empty or contains invalid JSON.")
+            app.vehicle_combo.configure(values=[])
+
+def get_odo_reading(app):
+    current_odometer_reading = app.odometer_entry.get()
+    vehicle_nickname = app.vehicle_combo.get()
+    file_name = f"Resources/{vehicle_nickname}_fill-ups.xlsx"
+    if os.path.isfile(file_name) and os.path.getsize(file_name) > 0:
+        fill_ups = pd.read_excel(file_name)
+        if not fill_ups.empty:
+            vehicle_fill_ups = fill_ups[fill_ups["Vehicle Nickname"] == vehicle_nickname]
+            if not vehicle_fill_ups.empty:
+                last_odometer = vehicle_fill_ups["Odometer"].astype(int).max()
+                if int(current_odometer_reading) <= last_odometer:
+                    print("New odometer reading must be greater than the previous reading. Please try again.")
+                    return False
+                else:
+                    app.odometer_entry.delete(0, tkinter.END)
+                    return True
+    return True
+
+def handle_submit_button(app):
+    odometer = app.odometer_entry.get()
+    vehicle_nickname = app.vehicle_combo.get()
+    insert_into_info_box(app, f"Currently selected vehicle: {vehicle_nickname}\n")
+    try:
+        odometer = int(odometer.strip())
+    except ValueError:
+        print("Odometer reading must be a number. Please try again.")
+        app.odometer_button.configure(fg_color="red")
+        app.odometer_entry.delete(0, tkinter.END)
+        return
+    file_name = f"Resources/{vehicle_nickname}_fill-ups.xlsx"
+    if os.path.isfile(file_name) and os.path.getsize(file_name) > 0:
+        fill_ups = pd.read_excel(file_name, engine="openpyxl")
+        if not fill_ups.empty:
+            fill_ups["Vehicle Nickname"] = fill_ups["Vehicle Nickname"].astype(str)
+            vehicle_fill_ups = fill_ups[fill_ups["Vehicle Nickname"] == vehicle_nickname]
+            if not vehicle_fill_ups.empty:
+                last_odometer = vehicle_fill_ups["Odometer"].astype(int).max()
+                if int(odometer) <= last_odometer:
+                    print("New odometer reading must be greater than the previous reading. Please try again.")
+                    app.odometer_button.configure(fg_color="red")
+                    insert_into_info_box(app, "New odometer reading must be greater than the previous reading. Please try again.\n")
+                    app.odometer_entry.delete(0, tkinter.END)
+                    return
+                else:
+                    app.odometer_button.configure(fg_color="blue")
+    else:
+        fill_ups = pd.DataFrame(columns=["Vehicle Nickname", "Date", "Odometer", "Gallons", "Gas Price", "Total Cost"])
+    fuel_price = app.fuel_price_entry.get()
+    try:
+        fuel_price = float(fuel_price)
+    except ValueError:
+        print("Fuel price must be a number. Please try again.")
+        app.fuel_price_button.configure(fg_color="red")
+        app.fuel_price_entry.delete(0, tkinter.END)
+        return
+    if fuel_price <= 0:
+        app.fuel_price_button.configure(fg_color="red")
+        print("Fuel price must be greater than zero. Please try again.")
+        app.fuel_price_entry.delete(0, tkinter.END)
+        return
+    else:
+        app.fuel_price_button.configure(fg_color="blue")
+    total_gallons = app.Entry3.get()
+    try:
+        total_gallons = float(total_gallons)
+    except ValueError:
+        print("Total gallons must be a number. Please try again.")
+        app.Entry3.delete(0, tkinter.END)
+        return
+    if total_gallons <= 0:
+        print("Total gallons must be greater than zero. Please try again.")
+        app.Entry3.delete(0, tkinter.END)
+        return
+    total_cost = app.total_cost_entry.get()
+    try:
+        total_cost = float(total_cost)
+    except ValueError:
+        print("Total cost must be a number. Please try again.")
+        app.total_cost_entry.delete(0, tkinter.END)
+        return
+    if total_cost <= 0:
+        print("Total cost must be greater than zero. Please try again.")
+        app.total_cost_entry.delete(0, tkinter.END)
+        return
+    date = app.date_combobox.get()
+    fill_up = {
+        "Vehicle Nickname": vehicle_nickname,
+        "Date": date,
+        "Odometer": odometer,
+        "Gallons": total_gallons,
+        "Gas Price": fuel_price,
+        "Total Cost": total_cost,
+    }
+    fill_ups = pd.concat([fill_ups, pd.DataFrame([fill_up])], ignore_index=True)
+    fill_ups.to_excel(file_name, index=False, engine="openpyxl")
+    app.odometer_entry.delete(0, tkinter.END)
+    app.fuel_price_entry.delete(0, tkinter.END)
+    app.Entry3.delete(0, tkinter.END)
+    app.total_cost_entry.delete(0, tkinter.END)
+
+def show_total_cost_for_vehicle(app):
+    vehicle_nickname = app.vehicle_combo.get()
+    file_name = f"Resources/{vehicle_nickname}_fill-ups.xlsx"
+    if os.path.isfile(file_name):
+        fill_ups = pd.read_excel(file_name)
+        total_cost = fill_ups["Total Cost"].sum()
+        insert_into_info_box(app, f"Statistics on  {vehicle_nickname}: \n${total_cost:.2f}\n{len(fill_ups)} fill-ups\n")
+    else:
+        insert_into_info_box(app, f"No data available for {vehicle_nickname}.\n")
+
+def calculate_fuel_efficiency(app, vehicle_nickname):
+    file_name = f"Resources/{vehicle_nickname}_fill-ups.xlsx"
+    if os.path.isfile(file_name):
+        fill_ups = pd.read_excel(file_name)
+        if not fill_ups.empty:
+            fill_ups["Odometer"] = fill_ups["Odometer"].astype(int)
+            fill_ups["Gallons"] = fill_ups["Gallons"].astype(float)
+            fill_ups.sort_values(by="Date", inplace=True)
+            fill_ups["Fuel Efficiency"] = (fill_ups["Odometer"].diff() / fill_ups["Gallons"].diff()).fillna(0)
+            insert_into_info_box(app, f"Fuel Efficiency for {vehicle_nickname}: \n{fill_ups['Fuel Efficiency'].mean():.2f} MPG\n")
+        else:
+            insert_into_info_box(app, f"No data available for {vehicle_nickname}.\n")
+    else:
+        insert_into_info_box(app, f"No data available for {vehicle_nickname}.\n")
---
+++ window.py
@@ -0,0 +1,161 @@
+import os
+import tkinter
+import customtkinter
+from picker import CTkDatePicker
+from add_vehicle import VehicleAdder
+from functions import insert_into_info_box, clear_info_box, load_vehicles, get_odo_reading, handle_submit_button, show_total_cost_for_vehicle, calculate_fuel_efficiency
+from tools import MoreTools
+import pandas as pd
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+class Window(customtkinter.CTk):
+    HEIGHT = 600
+    WIDTH = 800
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.title("Vehicle Fuel Efficiency Tracker")
+        self.geometry(f"{self.WIDTH}x{self.HEIGHT}")
+        self.resizable(False, False)
+
+        self.top_frame = customtkinter.CTkFrame(self, width=800, height=100)
+        self.top_frame.place(x=0, y=0)
+
+        self.Frame1 = customtkinter.CTkFrame(self, width=800, height=240)
+        self.Frame1.place(x=0, y=360)
+
+        self.odometer_button = customtkinter.CTkButton(
+            self,
+            bg_color=["gray86", "gray17"],
+            fg_color="blue",
+            text="Current Odometer Reading",
+            hover=False,
+            font=customtkinter.CTkFont("Roboto", size=13, weight="bold"),
+        )
+        self.odometer_button.place(x=80, y=380)
+
+        self.odometer_entry = customtkinter.CTkEntry(
+            self, bg_color=["gray86", "gray17"], width=160
+        )
+        self.odometer_entry.place(x=80, y=420)
+
+        self.fuel_price_button = customtkinter.CTkButton(
+            self,
+            bg_color=["gray86", "gray17"],
+            fg_color="blue",
+            text="Fuel Price",
+            hover=False,
+            width=160,
+            font=customtkinter.CTkFont("Roboto", size=13, weight="bold"),
+        )
+        self.fuel_price_button.place(x=80, y=480)
+
+        self.fuel_price_entry = customtkinter.CTkEntry(
+            self, bg_color=["gray86", "gray17"], width=160
+        )
+        self.fuel_price_entry.place(x=80, y=520)
+
+        self.total_gallons_button = customtkinter.CTkButton(
+            self,
+            bg_color=["gray86", "gray17"],
+            fg_color="blue",
+            text="Total Gallons Purchased",
+            hover=False,
+            width=160,
+            font=customtkinter.CTkFont("Roboto", size=13, weight="bold"),
+        )
+        self.total_gallons_button.place(x=320, y=380)
+
+        self.Entry3 = customtkinter.CTkEntry(
+            self, bg_color=["gray86", "gray17"], width=160
+        )
+        self.Entry3.place(x=320, y=420)
+
+        self.total_cost_button = customtkinter.CTkButton(
+            self,
+            bg_color=["gray86", "gray17"],
+            fg_color="blue",
+            text="Total Cost",
+            hover=False,
+            width=160,
+            font=customtkinter.CTkFont("Roboto", size=13, weight="bold"),
+        )
+        self.total_cost_button.place(x=320, y=480)
+
+        self.total_cost_entry = customtkinter.CTkEntry(
+            self, bg_color=["gray86", "gray17"], width=160
+        )
+        self.total_cost_entry.place(x=320, y=520)
+
+        self.more_button = customtkinter.CTkButton(
+            self,
+            bg_color=["gray86", "gray17"],
+            font=customtkinter.CTkFont("Roboto", size=26, weight="bold"),
+            height=50,
+            text="More",
+            fg_color="#00b900",
+            text_color="#000000",
+            command=lambda:self.open_more_tools,
+        )
+        self.more_button.place(x=500, y=545)
+
+        self.submit_button = customtkinter.CTkButton(
+            self,
+            bg_color=["gray86", "gray17"],
+            font=customtkinter.CTkFont("Roboto", size=26, weight="bold"),
+            height=50,
+            text="Submit",
+            fg_color="#00b900",
+            text_color="#000000",
+            command=lambda:self.handle_submit_button,
+        )
+        self.submit_button.place(x=655, y=545)
+
+        self.date_picker_label = customtkinter.CTkButton(
+            self,
+            bg_color=["gray86", "gray17"],
+            fg_color="blue",
+            text="Select a Date",
+            hover=False,
+            width=160,
+            font=customtkinter.CTkFont("Roboto", size=13, weight="bold"),
+        )
+        self.date_picker_label.place(x=550, y=380)
+
+        self.date_combobox = customtkinter.CTkComboBox(self)
+        self.date_combobox.place(x=550, y=420)
+
+        CTkDatePicker(self.date_combobox)
+
+        self.vehicle_combo = customtkinter.CTkComboBox(
+            self, values=[], bg_color=["gray86", "gray17"], justify="center"
+        )
+        self.vehicle_combo.place(x=10, y=35)
+
+        self.select_vehicle_label = customtkinter.CTkLabel(
+            self, text="Select a vehicle"
+        )
+        self.select_vehicle_label.place(x=30, y=2)
+
+        self.add_vehicle_button = customtkinter.CTkButton(
+            self,
+            bg_color=["gray86", "gray17"],
+            text="Add Vehicle",
+            command=lambda:self.open_vehicle_adder,
+        )
+        self.add_vehicle_button.place(x=650, y=10)
+
+        self.info_box = customtkinter.CTkTextbox(
+            self,
+            bg_color=["gray92", "gray14"],
+            width=792,
+            height=250,
+            corner_radius=5,
+            border_width=3,
+        )
+        self.info_box.place(x=4, y=105)
+        self.info_box.insert(1.0, "")
+
+        #self.load_vehicles()
