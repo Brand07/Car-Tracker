46de6afcb8603bc961438170e3c879443071a433
Brand07 <81128304+Brand07@users.noreply.github.com>
2025-02-19 20:20:13 -0600

    Create MoreTools class

---
+++ tools.py
@@ -0,0 +1,13 @@
+import customtkinter
+
+class MoreTools(customtkinter.CTkToplevel):
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs)
+        self.title("More Tools")
+        self.geometry("500x300")
+        self.resizable(False, False)
+        self.grab_set()
+
+        # Add a label to the window
+        label = customtkinter.CTkLabel(self, text="More Tools")
+        label.pack(pady=20)
